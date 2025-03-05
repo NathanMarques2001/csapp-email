38fc44781f716f1f1919030f7b1067c37e4e5734
NathanMarques2001 <nathanmarques20@yahoo.com.br>
2025-03-05 02:36:45 -0300

    feat: Adiciona suporte a CORS no aplicativo

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const emailRoutes = require('./routes/emailRoutes');
+const cors = require('cors');
 require('./config/dotenv');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use('/email', emailRoutes);
 
